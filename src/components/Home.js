77b28bd8d714bdaddc610c5ab8ed613ab2ba9937
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-02-22 16:47:12 -0600

    basic app.js things before restarting computer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome to Swapi Fun!</h1>
+      <h2>Where we have fun with Swapi</h2>
+    </div>
+  );
 };
 
 export default Home;
