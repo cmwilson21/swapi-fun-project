a15e0767e1d9a814197dac308c0cfbdb9460bda5
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-02-22 17:20:54 -0600

    added basic routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>This is the nav bar</div>;
+  return (
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/people">People</NavLink>
+      <NavLink to="/planets">Planets</NavLink>
+      <NavLink to="/starships">Starships</NavLink>
+    </div>
+  );
 };
 
 export default NavBar;
