3f2d0473012a169bb4192ee2b0813001f99ba300
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-02-22 20:45:25 -0600

    moved some stuff around to the lists

--- src/components/lists/PeopleList.js
+++ src/components/lists/PeopleList.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { PeopleCard } from "../cards/PeopleCard";
 
 export const PeopleList = () => {
+  const swapiUrl = "https://swapi.dev/api/people/";
+
+  // const swGetter = useEffect(() => {
+  //   fetch(swapiUrl)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
+  // create a function that will fetch the data from the swapi url
+  // and return the data as a json object
+  const swGetter = () => {
+    fetch(swapiUrl)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  useEffect(() => {
+    swGetter();
+  }, []);
+
+  console.log("line 25", swGetter[1]);
+
   return (
     <div>
       People List
