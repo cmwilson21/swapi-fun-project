520720734d657194aa59fd8062517d05dd97aa6e
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-03-31 15:20:48 -0500

    basic logic on the planet list page

--- src/components/lists/PlanetsList.js
+++ src/components/lists/PlanetsList.js
@@ -1,7 +1,32 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { PlanetCard } from "../cards/PlanetCard";
 
 export const PlanetsList = () => {
+  // set the planets state
+  const [planets, setPlanets] = useState([]);
+
+  // grab the planets from the api https://swapi.dev/api/planets/ and store it in a variable called planets
+  const swapiUrl = "https://swapi.dev/api/planets/";
+
+  // create a function that will fetch the data from the swapi url
+  // and return the data as a json object
+  const swGetter = () => {
+    fetch(swapiUrl)
+      .then((res) => res.json())
+      .then((data) => console.log("data", data));
+  };
+
+  useEffect(() => {
+    swGetter();
+  }, []);
+
+  console.log("line 25", swGetter[1]);
+
+  // map through the planets array and return a div with the name of the planet
+  planets.map((planet) => {
+    return <div>{planet.name}</div>;
+  });
+
   return (
     <div>
       PlanetsList
