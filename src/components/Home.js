914ade35798b3a4d65d33d7d793c54c7c9a6c610
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-02 21:29:40 -0500

    centered text on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,30 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Swapi Fun!</h1>
-      <h2>Where we have fun with Swapi</h2>
+      <h1
+        style={{
+          color: "gray",
+          textAlign: "center",
+          marginLeft: "auto",
+          marginRight: "auto",
+
+          padding: 20,
+        }}
+      >
+        Welcome to Swapi Fun!
+      </h1>
+      <h2
+        style={{
+          color: "gray",
+          textAlign: "center",
+          marginLeft: "auto",
+          marginRight: "auto",
+
+          padding: 20,
+        }}
+      >
+        Where we have fun with Swapi
+      </h2>
     </div>
   );
 };
