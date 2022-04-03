3c2ae2c1ba47bc2929e087c5bcb07e502f5ce0c1
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-02 21:33:03 -0500

    added unsplash image

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,12 @@ const Home = () => {
       >
         Welcome to Swapi Fun!
       </h1>
+
+      <img
+        src="https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+        alt="Lego Stormtrooper walking in sand"
+      />
+
       <h2
         style={{
           color: "gray",
